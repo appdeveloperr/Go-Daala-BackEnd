c7748fe23b16295a27b303b77f726426f5df2a10
firdousahmadkhosa <68013808+firdousahmadkhosa@users.noreply.github.com>
2021-04-23 16:01:24 +0500

    fileuploading

--- routes/admin_routes/banner.routes.js
+++ routes/admin_routes/banner.routes.js
@@ -5,6 +5,8 @@ var banner_controller = require('../../controllers/admin_controllers/banner');
 
 const multer = require('multer');
 var path = require('path');
+const express = require("express");
+
 
 const fs = require('fs');
 module.exports = function (app) {
@@ -25,27 +27,61 @@ module.exports = function (app) {
 
 
   // SET STORAGE
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/files/uploadsFiles/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-  });
+  // const storage = multer.diskStorage({
+  //   destination: function (req, file, cb) {
+  //     console.log(file);
+  //     cb(null, './public/files/uploadsFiles/')
+  //   },
+  //   filename: function (req, file, cb) {
+  //     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  //   }
+  // });
 
-  const fileFilter = (req, file, cb) => {
-    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/jpg') {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
+  // const fileFilter = (req, file, cb) => {
+  //   if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/jpg') {
+  //     cb(null, true);
+  //   } else {
+  //     cb(null, false);
+  //   }
+  // }
+
+
+  //Storage Engine
+const storage = multer.diskStorage({
+  destination: './upload/images/userProfile',
+  filename: (req, file, cb) => {
+      return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
   }
+})
 
-  const uploads = multer({ storage: storage, fileFilter: fileFilter });
+const upload = multer({
+  storage: storage,
+});
+
+
+
+// const multer = require("multer");
+// const path  = require("path");
+
+
+
+  // const uploads = multer({ storage: storage, fileFilter: fileFilter });
+
+
+
+  app.use('/profile', express.static('upload/images/userProfile'));
+
+
+  app.post("/admin/banner/upload", 
+  isAdmin,
+   upload.single('myFile'), 
+   banner_controller.create
+   );
+
+
 
 //------------------post banner create-----------------------------------------
-  app.post("/admin/banner/upload", isAdmin, uploads.single('myFile'), banner_controller.create);
+  // app.post("/admin/banner/upload", isAdmin, uploads.single('myFile'), banner_controller.create);
 
 //------------------get banner edit  && redirect to get banner edit  To next route -----------------------------------------
   app.get("/admin/banner/Edit/:id", isAdmin, function (req, res) {
@@ -58,7 +94,7 @@ module.exports = function (app) {
   app.get('/admin/Edit_bannar/:id',isAdmin, banner_controller.edit);
 
   //------------------post banner update---------------------------------
-  app.post("/admin/banner/update", isAdmin,uploads.single('myFile'),banner_controller.update);
+  app.post("/admin/banner/update", isAdmin,upload.single('myFile'),banner_controller.update);
     
 
   //-----------------get banner delete ---------------------
