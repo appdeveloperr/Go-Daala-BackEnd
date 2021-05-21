8c333b17fbcd4846b183addef923fb91a491f383
Usman Sh <usman.shahzadd@gmail.com>
2021-05-21 14:14:51 +0500

    Revert "Update vehicle_reg.js"
    This reverts commit 445929d66206bd7c699438567db0efaa487e487d.

--- controllers/api_controllers/driver/vehicle_reg.js
+++ controllers/api_controllers/driver/vehicle_reg.js
@@ -8,202 +8,139 @@ const fs = require('fs');
 //-------------Driver create vehicle register--------------------
 exports.create_vehicle_reg = (req, res) => {
 
+    req.checkBody('driver_id', 'driver_id must have ID!').notEmpty();
+    req.checkBody('vehicle_type', 'vehicle_type must have value!').notEmpty();
+    req.checkBody('brand', 'brand must have value!').notEmpty();
+    req.checkBody('model', 'model must have value!').notEmpty();
+    req.checkBody('number_plate', 'number_plate must have value!').notEmpty();
+    req.checkBody('color', 'color must have value!').notEmpty();
 
-
-
-
-    // Save vendor to Database
-    var docfilename = "";
-    var frintfilename = "";
-    var backfilename = "";
-
-    Vehicle_reg.create({
-        driver_id: req.body.driver_id,
-        vehicle_type: req.body.vehicle_type,
-        vehicle_document: '/files/uploadsFiles/driver/' + docfilename,
-        brand: req.body.brand,
-        model: req.body.model,
-        number_plate: req.body.number_plate,
-        color: req.body.color,
-        frint_image: '/files/uploadsFiles/driver/' + frintfilename,
-        back_image: '/files/uploadsFiles/driver/' + backfilename,
-        status: "deactive",
-
-
-    }).then(vehicle_reg => {
-        Vehicle.findOne({ where: { vehicle_type: vehicle_reg.vehicle_type } }).then(vehicles => {
-
-
-            return res.status(200).send({
-                status: 200,
-                message: "Driver create Vehicle register is successful",
-                successData: {
-                    vehicle: {
-                        id: vehicle_reg.id,
-                        vehicle_type: vehicle_reg.vehicle_type,
-                        vehicle_document: vehicle_reg.vehicle_document,
-                        brand: vehicle_reg.brand,
-                        model: vehicle_reg.model,
-                        number_plate: vehicle_reg.number_plate,
-                        color: vehicle_reg.color,
-                        frint_image: vehicle_reg.frint_image,
-                        back_image: vehicle_reg.back_image,
-                        status: vehicle_reg.status,
-                        driver_id: vehicle_reg.driver_id,
-                        service: vehicles.service,
-                        distance: vehicles.distance,
-                        time: vehicles.time
-                    }
+    var errors = req.validationErrors();
+    if (errors) {                    //////////------input text validation error
+        return res.status(200).send({
+            status: 400,
+            message: "validation error in vehicle register",
+            successData: {
+                error: {
+                    error: errors
                 }
-            });
-        })
-
-
-    })
-        .catch(err => {
-
-            return res.status(200).send({
-                status: 400,
-                message: err.message,
-                successData: {}
-            });
-
+            }
         });
+    } else {
+        if (!req.files) {
+            req.checkBody('vehicle_document', 'vehicle document picture must have needed!').notEmpty();
+            req.checkBody('frint_image', 'frint image must have needed!').notEmpty();
+            req.checkBody('back_image', 'back image License picture must have needed!').notEmpty();
+            var errors = req.validationErrors();
+            if (errors) {                    //////////------input file validation error
+                return res.status(200).send({
+                    status: 400,
+                    message: "validation error in vehicle register",
+                    successData: {
+                        error: {
+                            error: errors
+                        }
+                    }
+                });
+            }
+        } else {
+            req.checkBody('vehicle_document', 'vehicle_document picture must have needed animage').isImage(req.files.vehicle_document.name);
+            req.checkBody('frint_image', 'frint_image  must have needed animage').isImage(req.files.frint_image.name);
+            req.checkBody('back_image', 'back image  must have needed animage').isImage(req.files.back_image.name);
+            var errors = req.validationErrors();
+            if (errors) {   //////////------input file must have image validation error
+                return res.status(200).send({
+                    status: 400,
+                    message: "validation error in vehicle register",
+                    successData: {
+                        error: {
+                            error: errors
+                        }
+                    }
+                });
+            } else {   ///------------------ no error exist
+
+                var path_file = './public/files/uploadsFiles/driver/';
 
 
-    // req.checkBody('driver_id', 'driver_id must have ID!').notEmpty();
-    // req.checkBody('vehicle_type', 'vehicle_type must have value!').notEmpty();
-    // req.checkBody('brand', 'brand must have value!').notEmpty();
-    // req.checkBody('model', 'model must have value!').notEmpty();
-    // req.checkBody('number_plate', 'number_plate must have value!').notEmpty();
-    // req.checkBody('color', 'color must have value!').notEmpty();
+                //-----------------move vehicle document into server-------------------------------//
+                var docfilename = 'profile-1' + Date.now() + req.files.vehicle_document.name;
+                req.files.vehicle_document.mv(path_file + '' + docfilename, function (err) {
+                    if (err) console.log("error occured");
+                });
 
-    // var errors = req.validationErrors();
-    // if (errors) {                    //////////------input text validation error
-    //     return res.status(200).send({
-    //         status: 400,
-    //         message: "validation error in vehicle register",
-    //         successData: {
-    //             error: {
-    //                 error: errors
-    //             }
-    //         }
-    //     });
-    // } else {
-    //     if (!req.files) {
-    //         req.checkBody('vehicle_document', 'vehicle document picture must have needed!').notEmpty();
-    //         req.checkBody('frint_image', 'frint image must have needed!').notEmpty();
-    //         req.checkBody('back_image', 'back image License picture must have needed!').notEmpty();
-    //         var errors = req.validationErrors();
-    //         if (errors) {                    //////////------input file validation error
-    //             return res.status(200).send({
-    //                 status: 400,
-    //                 message: "validation error in vehicle register",
-    //                 successData: {
-    //                     error: {
-    //                         error: errors
-    //                     }
-    //                 }
-    //             });
-    //         }
-    //     } else {
-    //         req.checkBody('vehicle_document', 'vehicle_document picture must have needed animage').isImage(req.files.vehicle_document.name);
-    //         req.checkBody('frint_image', 'frint_image  must have needed animage').isImage(req.files.frint_image.name);
-    //         req.checkBody('back_image', 'back image  must have needed animage').isImage(req.files.back_image.name);
-    //         var errors = req.validationErrors();
-    //         if (errors) {   //////////------input file must have image validation error
-    //             return res.status(200).send({
-    //                 status: 400,
-    //                 message: "validation error in vehicle register",
-    //                 successData: {
-    //                     error: {
-    //                         error: errors
-    //                     }
-    //                 }
-    //             });
-    //         } else {   ///------------------ no error exist
-
-    //             var path_file = './public/files/uploadsFiles/driver/';
-
-
-    //             //-----------------move vehicle document into server-------------------------------//
-    //             var docfilename = 'profile-1' + Date.now() + req.files.vehicle_document.name;
-    //             req.files.vehicle_document.mv(path_file + '' + docfilename, function (err) {
-    //                 if (err) console.log("error occured");
-    //             });
-
-    //             //-----------------move frint_image into server-------------------------------//
-    //             var frintfilename = 'profile-2' + Date.now() + req.files.frint_image.name;
-    //             req.files.frint_image.mv(path_file + '' + frintfilename, function (err) {
-    //                 if (err) console.log("error occured");
-    //             });
+                //-----------------move frint_image into server-------------------------------//
+                var frintfilename = 'profile-2' + Date.now() + req.files.frint_image.name;
+                req.files.frint_image.mv(path_file + '' + frintfilename, function (err) {
+                    if (err) console.log("error occured");
+                });
 
 
 
-    //             //-----------------move back_image into server-------------------------------//
-    //             var backfilename = 'profile-3' + Date.now() + req.files.back_image.name;
-    //             req.files.back_image.mv(path_file + '' + backfilename, function (err) {
-    //                 if (err) console.log("error occured");
-    //             });
+                //-----------------move back_image into server-------------------------------//
+                var backfilename = 'profile-3' + Date.now() + req.files.back_image.name;
+                req.files.back_image.mv(path_file + '' + backfilename, function (err) {
+                    if (err) console.log("error occured");
+                });
 
 
 
-    //             // Save vendor to Database
-    //             Vehicle_reg.create({
-    //                 driver_id: req.body.driver_id,
-    //                 vehicle_type: req.body.vehicle_type,
-    //                 vehicle_document: '/files/uploadsFiles/driver/' + docfilename,
-    //                 brand: req.body.brand,
-    //                 model: req.body.model,
-    //                 number_plate: req.body.number_plate,
-    //                 color: req.body.color,
-    //                 frint_image: '/files/uploadsFiles/driver/' + frintfilename,
-    //                 back_image: '/files/uploadsFiles/driver/' + backfilename,
-    //                 status: "deactive",
+                // Save vendor to Database
+                Vehicle_reg.create({
+                    driver_id: req.body.driver_id,
+                    vehicle_type: req.body.vehicle_type,
+                    vehicle_document: '/files/uploadsFiles/driver/' + docfilename,
+                    brand: req.body.brand,
+                    model: req.body.model,
+                    number_plate: req.body.number_plate,
+                    color: req.body.color,
+                    frint_image: '/files/uploadsFiles/driver/' + frintfilename,
+                    back_image: '/files/uploadsFiles/driver/' + backfilename,
+                    status: "deactive",
 
 
-    //             }).then(vehicle_reg => {
-    // 				Vehicle.findOne({where:{vehicle_type:vehicle_reg.vehicle_type}}).then(vehicles=>{
+                }).then(vehicle_reg => {
+					Vehicle.findOne({where:{vehicle_type:vehicle_reg.vehicle_type}}).then(vehicles=>{
+				
+						
+					  return res.status(200).send({
+                        status: 200,
+                        message: "Driver create Vehicle register is successful",
+                        successData: {
+                            vehicle: {
+                                id: vehicle_reg.id,
+                                vehicle_type: vehicle_reg.vehicle_type,
+                                vehicle_document: vehicle_reg.vehicle_document,
+                                brand: vehicle_reg.brand,
+                                model: vehicle_reg.model,
+                                number_plate: vehicle_reg.number_plate,
+                                color: vehicle_reg.color,
+                                frint_image: vehicle_reg.frint_image,
+                                back_image: vehicle_reg.back_image,
+                                status: vehicle_reg.status,
+                                driver_id: vehicle_reg.driver_id,
+								service:vehicles.service,
+								distance:vehicles.distance,
+								time: vehicles.time
+                            }
+                        }
+                    });	
+					})
+                  
 
+                })
+                    .catch(err => {
 
-    // 				  return res.status(200).send({
-    //                     status: 200,
-    //                     message: "Driver create Vehicle register is successful",
-    //                     successData: {
-    //                         vehicle: {
-    //                             id: vehicle_reg.id,
-    //                             vehicle_type: vehicle_reg.vehicle_type,
-    //                             vehicle_document: vehicle_reg.vehicle_document,
-    //                             brand: vehicle_reg.brand,
-    //                             model: vehicle_reg.model,
-    //                             number_plate: vehicle_reg.number_plate,
-    //                             color: vehicle_reg.color,
-    //                             frint_image: vehicle_reg.frint_image,
-    //                             back_image: vehicle_reg.back_image,
-    //                             status: vehicle_reg.status,
-    //                             driver_id: vehicle_reg.driver_id,
-    // 							service:vehicles.service,
-    // 							distance:vehicles.distance,
-    // 							time: vehicles.time
-    //                         }
-    //                     }
-    //                 });	
-    // 				})
+                        return res.status(200).send({
+                            status: 400,
+                            message: err.message,
+                            successData: {}
+                        });
 
-
-    //             })
-    //                 .catch(err => {
-
-    //                     return res.status(200).send({
-    //                         status: 400,
-    //                         message: err.message,
-    //                         successData: {}
-    //                     });
-
-    //                 });
-    //         }
-    //     }
-    // }
+                    });
+            }
+        }
+    }
 };
 
 //-------------Driver create vehicle register--------------------
@@ -239,8 +176,8 @@ exports.get_all_vehicles = (req, res) => {
 }
 
 //-------------Driver update vehicle if  driver is unblock  ----------------------
-exports.update_vehicle_reg = (req, res) => {
-    req.checkBody('id', 'Vehicle id must have required');
+exports.update_vehicle_reg=(req,res)=>{
+    req.checkBody('id','Vehicle id must have required');
     req.checkBody('driver_id', 'driver_id must have ID!').notEmpty();
     req.checkBody('vehicle_type', 'vehicle_type must have value!').notEmpty();
     req.checkBody('brand', 'brand must have value!').notEmpty();
@@ -292,13 +229,11 @@ exports.update_vehicle_reg = (req, res) => {
                     }
                 });
             } else {   ///------------------ no error exist
-                Vehicle_reg.findOne({
-                    where: {
-                        id: req.body.id
-                    }
-                }).then(result => {
-
-                    if (result != null || result != '') {
+                Vehicle_reg.findOne({where:{
+                    id:req.body.id
+                }}).then(result=>{
+                   
+                    if(result!=null|| result!=''){
 
                         fs.unlink('./public' + result.dataValues.vehicle_document, function (err) {
                             if (err) {
@@ -334,23 +269,23 @@ exports.update_vehicle_reg = (req, res) => {
                         req.files.vehicle_document.mv(path_file + '' + docfilename, function (err) {
                             if (err) console.log("error occured");
                         });
-
+        
                         //-----------------move frint_image into server-------------------------------//
                         var frintfilename = 'profile-2' + Date.now() + req.files.frint_image.name;
                         req.files.frint_image.mv(path_file + '' + frintfilename, function (err) {
                             if (err) console.log("error occured");
                         });
-
-
-
+        
+        
+        
                         //-----------------move back_image into server-------------------------------//
                         var backfilename = 'profile-3' + Date.now() + req.files.back_image.name;
                         req.files.back_image.mv(path_file + '' + backfilename, function (err) {
                             if (err) console.log("error occured");
                         });
-
-
-
+        
+        
+        
                         // Save vendor to Database
                         Vehicle_reg.update({
                             driver_id: req.body.driver_id,
@@ -363,11 +298,11 @@ exports.update_vehicle_reg = (req, res) => {
                             frint_image: '/files/uploadsFiles/driver/' + frintfilename,
                             back_image: '/files/uploadsFiles/driver/' + backfilename,
                             status: "deactive",
-
-
-                        }, {
-                            where: {
-                                id: result.dataValues.id
+        
+        
+                        },{
+                            where:{
+                                id:result.dataValues.id
                             },
                             returning: true,
                             plain: true
@@ -391,31 +326,30 @@ exports.update_vehicle_reg = (req, res) => {
                                     }
                                 }
                             });
-
+        
                         })
                             .catch(err => {
-
+        
                                 return res.status(200).send({
                                     status: 400,
                                     message: err.message,
                                     successData: {}
                                 });
-
+        
                             });
-                    } else {
+                    }else{
                         return res.status(200).send({
                             status: 200,
                             message: "Driver create Vehicle register is successful",
                             successData: {
-                                vehicle: {
-                                    result
-
+                                vehicle: { result
+                                 
                                 }
                             }
                         });
                     }
                 })
-
+             
             }
         }
     }
