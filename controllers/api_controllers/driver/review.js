7c6b0ca6785945b3095d1bab30f0d6c9a4751a7e
firdousahmadkhosa <68013808+firdousahmadkhosa@users.noreply.github.com>
2021-05-25 13:17:43 +0500

    updated

--- controllers/api_controllers/driver/review.js
+++ controllers/api_controllers/driver/review.js
@@ -239,7 +239,6 @@ exports.get_review = (req, res) => {
                             status: 200,
                             message: "get driver reviews   is successful",
                             successData: {
-                                reviews: reviews.dataValues,
                                 trip: trip.dataValues
 
                             }
