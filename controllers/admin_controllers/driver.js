4f5502605e7ae78e773e9b2026bd562ff1ee5ae3
firdousahmadkhosa <68013808+firdousahmadkhosa@users.noreply.github.com>
2021-07-12 11:08:19 +0500

    updated

--- controllers/admin_controllers/driver.js
+++ controllers/admin_controllers/driver.js
@@ -299,95 +299,39 @@ exports.unactive = function (req, res, next) {
 
 //--------------driver recent  all trip---------------
 exports.recent_trip = (req, res) => {
-
-  Vehicle_reg.findOne({
+  Driver.findOne({
     where: {
-      driver_id: req.body.params
-    },include:[
-      {
-        model: db.driver
-      }
-    ]
-  }).then(driver_data => {
-    if (driver_data==null || driver_data=='') {
-      console.log("driver is data is empty");
-      // Driver.findOne({
-      //   where: {
-      //     id: req.params.id
-      //   }
-      // }).then(driver => {
-      //   Trip.findAll({
-      //     where: {
-      //       driver_id: req.params.id
-      //     },
-      //     include: [
-      //       {
-      //         model: db.vendor
-      //       },
-      //       {
-      //         model: db.customer
-      //       }
-      //     ]
-      //   }).then(trip => {
-      //     return res.status(200).send({
-      //       status: 200,
-      //       message: "",
-      //       successData: {
-      //         trips: trip
-      //       }
-      //     });
-      //   }).catch(err => {
-
-      //     return res.status(200).send({
-      //       status: 400,
-      //       message: err.message,
-      //       successData: {}
-      //     });
-      //   });
-      // });
-    } else {
-      console.log("driver is data is not empty"+driver_data);
-      res.send(driver_data);
-      // Trip.findAll({
-      //   where: {
-      //     driver_id: req.params.id
-      //   },
-      //   include: [
-      //     {
-      //       model: db.vendor
-      //     },
-      //     {
-      //       model: db.customer
-      //     }
-      //   ]
-      // }).then(trip => {
-      //   return res.status(200).send({
-      //     status: 200,
-      //     message: "",
-      //     successData: {
-      //       trips: trip
-      //     }
-      //   });
-      // }).catch(err => {
-
-      //   return res.status(200).send({
-      //     status: 400,
-      //     message: err.message,
-      //     successData: {}
-      //   });
-
-      // });
+      id: req.params.id
     }
-  }).catch (err => {
-
-  return res.status(200).send({
-    status: 400,
-    message:"driver is data is empty"+ err.message,
-    successData: {}
+  }).then(driver => {
+    Trip.findAll({
+      where: {
+        driver_id: req.params.id
+      },
+      include: [
+        {
+          model: db.vendor
+        },
+        {
+          model: db.customer
+        }
+      ]
+    }).then(trip => {
+      return res.status(200).send({
+        status: 200,
+        message: "",
+        successData: {
+          trips: trip
+        }
+      });
+    }).catch(err => {
+      return res.status(200).send({
+        status: 400,
+        message: err.message,
+        successData: {}
+      });
+    });
   });
-
-});
-
 }
 
 
