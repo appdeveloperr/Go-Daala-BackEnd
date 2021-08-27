3bcf2011f94db55e036ec0142be921c7605f47c3
Usman Sh <usman.shahzadd@gmail.com>
2021-08-27 14:35:19 +0500

    Update auth.js

--- controllers/api_controllers/driver/auth.js
+++ controllers/api_controllers/driver/auth.js
@@ -121,32 +121,30 @@ exports.signup = (req, res) => {
 
 
                      //Send Welcome SMS
-                     var messageData = encodeURI("لالا  کو ڈالا پر خوش آمدید کہتے ہیں  او لالا، چلاؤ ڈالا");
+                     var messageData = encodeURI( "لالا "+ req.body.first_name+" "+ req.body.last_name+" کو ڈالا پر خوش آمدید کہتے ہیں  او لالا، چلاؤ ڈالا");
                      var mobileno = req.body.phone_number;
  
                      axios.get('http://api.veevotech.com/sendsms?hash=2fefa107d5eddd16fc16e420e976b2eb&receivenum=' + mobileno + '&sendernum=8583&textmessage=' + messageData)
                          .then(response => {
  
-                            console.log("Driver Message Send Success");
-
-                            // return res.status(200).send({
-                            //     status: 200,
-                            //     message: "Signing Up is successful",
-                            //     successData: {
-                            //         user: user,
-                            //     }
-                            // });
+ 
+                            return res.status(200).send({
+                                status: 200,
+                                message: "Signing Up is successful",
+                                successData: {
+                                    user: user,
+                                }
+                            });
                          })
                          .catch(error => {
-                            console.log("Driver Message Send Success");
-                            console.log(error);
-                            // return res.status(200).send({
-                            //     status: 200,
-                            //     message: "Signing Up is successful",
-                            //     successData: {
-                            //         user: user,
-                            //     }
-                            // });
+                            
+                            return res.status(200).send({
+                                status: 200,
+                                message: "Signing Up is successful",
+                                successData: {
+                                    user: user,
+                                }
+                            });
                          });
 
                  
